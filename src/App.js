b2601a0d0927bad4080719f6ab0da0ccfd0d230a
Morgan Galvin <morgangalvin@yahoo.com>
2018-12-17 17:14:13 +1300

    first commit

--- src/App.js
+++ src/App.js
@@ -2,27 +2,58 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+class App extends Component{
+  constructor(){
+    super();
+  }
+  render(){
+    return(
+      <div>
+        <Machine />
       </div>
-    );
+    )
   }
 }
-
 export default App;
+
+class Machine extends Component{
+  constructor(){
+    super();
+  }
+  render(){
+    return(
+      <div id='drum-machine'>
+        <div id='pad-grid'>
+          <div id='Q' className='drum-pad'>
+            <p>Q</p>
+          </div>
+          <div id='W' className='drum-pad'>
+            <p>W</p>
+          </div>
+          <div id='E' className='drum-pad'>
+            <p>E</p>
+          </div>
+          <div id='A' className='drum-pad'>
+            <p>A</p>
+          </div>
+          <div id='S' className='drum-pad'>
+            <p>S</p>
+          </div>
+          <div id='D' className='drum-pad'>
+            <p>D</p>
+          </div>
+          <div id='Z' className='drum-pad'>
+            <p>Z</p>
+          </div>
+          <div id='X' className='drum-pad'>
+            <p>X</p>
+          </div>
+          <div id='C' className='drum-pad'>
+            <p>C</p>
+          </div>
+        </div>
+        <p id='display'>DISPLAY SOMETHING</p>
+      </div>
+    )
+  }
+}
