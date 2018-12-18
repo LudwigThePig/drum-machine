cdb41b9dcfdaa3bfe9c44584b90f89f4fb7636df
Morgan Galvin <morgangalvin@yahoo.com>
2018-12-18 13:17:04 +1300

    create drumpad object and sourced samples

--- src/App.js
+++ src/App.js
@@ -2,6 +2,60 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const drumpad = [
+  {
+    key: 'q',
+    keycode: 81,
+    id: 'string 1',
+    sound: 'https://s3-ap-southeast-2.amazonaws.com/fcc-wav/String+1.wav'
+  },
+  {
+    key: 'w',
+    keycode: 87,
+    id: 'string 2',
+    sound: 'https://s3-ap-southeast-2.amazonaws.com/fcc-wav/String+2.wav'
+  },
+  {
+    key: 'e',
+    keycode: 69,
+    id: 'string 3',
+    sound: 'https://s3-ap-southeast-2.amazonaws.com/fcc-wav/String+3.wav'
+  },
+  {
+    key: 'a',
+    keycode: 65,
+    id: 'string 4',
+    sound: 'https://s3-ap-southeast-2.amazonaws.com/fcc-wav/String+4.wav'
+  },
+  {
+    key: 's',
+    keycode: 83,
+    id: 'string 5',
+    sound: 'https://s3-ap-southeast-2.amazonaws.com/fcc-wav/String+5.wav'
+  },
+  {
+    key: 'd',
+    keycode: 68,
+    id: 'string 6',
+    sound: 'https://s3-ap-southeast-2.amazonaws.com/fcc-wav/String+6.wav'
+  },  {
+    key: 'z',
+    keycode: 90,
+    id: 'string 7',
+    sound: 'https://s3-ap-southeast-2.amazonaws.com/fcc-wav/String+7.wav'
+  },  {
+    key: 'x',
+    keycode: 88,
+    id: 'string 8',
+    sound: 'https://s3-ap-southeast-2.amazonaws.com/fcc-wav/String+8.wav'
+  },  {
+    key: 'c',
+    keycode: 67,
+    id: 'string 9',
+    sound: 'https://s3-ap-southeast-2.amazonaws.com/fcc-wav/String+9.wav'
+  },
+]
+
 class App extends Component{
   constructor(){
     super();
@@ -21,6 +75,7 @@ class Machine extends Component{
     super();
   }
   render(){
+
     return(
       <div id='drum-machine'>
         <div id='pad-grid'>
